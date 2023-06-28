485d5b4c518bbe44402ba450b453c449ccbaa14e
JayoungKim <jayoung.kim@yonsei.ac.kr>
2023-06-28 14:32:18 +0000

    first

---
+++ evaulation.py
@@ -0,0 +1,69 @@
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder
+
+
+CATEGORICAL = "categorical"
+CONTINUOUS = "continuous"
+ORDINAL = "ordinal"
+
+
+class FeatureMaker:
+
+    def __init__(self, metadata, label_column='label', label_type='int', sample=50000):
+        self.columns = metadata['columns']
+        self.label_column = label_column
+        self.label_type = label_type
+        self.sample = sample
+        self.encoders = dict()
+
+    def make_features(self, data):
+        data = data.copy()
+        np.random.shuffle(data)
+        data = data[:self.sample]
+
+        features = []
+        labels = []
+
+        for index, cinfo in enumerate(self.columns):
+            col = data[:, index]
+            if cinfo['name'] == self.label_column:
+                if self.label_type == 'int':
+                    labels = col.astype(int)
+                elif self.label_type == 'float':
+                    labels = col.astype(float)
+                else:
+                    assert 0, 'unkown label type'
+                continue
+
+            if cinfo['type'] == CONTINUOUS:
+                cmin = cinfo['min']
+                cmax = cinfo['max']
+                if cmin >= 0 and cmax >= 1e3:
+                    feature = np.log(np.maximum(col, 1e-2))
+
+                else:
+                    feature = (col - cmin) / (cmax - cmin) * 5
+
+            elif cinfo['type'] == ORDINAL:
+                feature = col
+
+            else:
+                if cinfo['size'] <= 2:
+                    feature = col
+
+                else:
+                    encoder = self.encoders.get(index)
+                    col = col.reshape(-1, 1)
+                    if encoder:
+                        feature = encoder.transform(col)
+                    else:
+                        encoder = OneHotEncoder(sparse=False, handle_unknown='ignore')
+                        self.encoders[index] = encoder
+                        feature = encoder.fit_transform(col)
+
+            features.append(feature)
+
+        features = np.column_stack(features)
+
+        return features, labels
+
